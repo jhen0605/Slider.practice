bd71b08b5c01ccaca72d807128ad0574c1be77c7
jhen0605 <amy95173@gmail.com>
2021-04-02 04:11:32 +0800

    Initial Commit

---
+++ Slider練習/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Slider練習
+//
+//  Created by 簡吟真 on 2021/4/2.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
