646f433e7553846c4dbcd8a7e930ef1815c39267
jhen0605 <amy95173@gmail.com>
2021-04-05 20:40:17 +0800

    1

--- Slider練習/ViewController.swift
+++ Slider練習/ViewController.swift
@@ -9,11 +9,50 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    //IBO
+    @IBOutlet weak var harryPotter: UIImageView!
+    @IBOutlet weak var portKey: UIImageView!
+    @IBOutlet weak var deathlyHallows: UIImageView!
+    @IBOutlet weak var owl: UIImageView!
+    @IBOutlet weak var schoolBadge: UIImageView!
+    @IBOutlet weak var goldenSnitch: UIImageView!
+    @IBOutlet weak var slider: UISlider!
+    @IBOutlet weak var dementor1: UIImageView!
+    @IBOutlet weak var dementor2: UIImageView!
+    
+    //貓頭鷹繞圈速度、位置
+    func updateLocation(degrees: CGFloat) {
+        owl.transform = CGAffineTransform.identity.rotated(by: CGFloat.pi/180*degrees).translatedBy(x: 20, y: -250)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
+        
+    //哈利波特、港口鑰的水平移動與滑軌設定
+    @IBAction func slider(_ sender: UISlider){
+        let speed = sender.value * 0.4
+        
+        harryPotter.frame.origin.x = CGFloat(464-speed)
+        portKey.frame.origin.x = CGFloat(118+speed)
 
-
-}
-
+        
+    //催狂魔的垂直移動、移動速度
+        dementor1.frame.origin.y = CGFloat(42-speed/6)
+        dementor2.frame.origin.y = CGFloat(283-speed/5)
+    
+        
+    //漸出的死神聖物
+        deathlyHallows.alpha = CGFloat(sender.value/360)
+        
+    //漸出的金探子
+        goldenSnitch.alpha = CGFloat(sender.value/360)
+        
+    //漸出的校徽
+        schoolBadge.alpha = CGFloat(sender.value/360)
+        
+        
+    //貓頭鷹繞圈角度
+        updateLocation(degrees: CGFloat(sender.value/2)+270)
+        }
+    }
